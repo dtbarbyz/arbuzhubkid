13b0586db00168ab6cacad5b55e4e19a01998f2b
dtbarbyz <100335015+dtbarbyz@users.noreply.github.com>
2023-03-11 14:05:01 +0300

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -12049,3 +12049,17 @@ end
  
 Mouse.KeyDown:Connect(TalkTrash)
 end)
+
+Section:NewButton("Click tp tool", "ButtonInfo", function()
+	mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "TpTool"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
